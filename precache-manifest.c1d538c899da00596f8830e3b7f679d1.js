1025dd2b4b277937b96300a5689af7f675db10a7
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2020-05-29 10:32:45 +0200

    Updates

---
+++ precache-manifest.c1d538c899da00596f8830e3b7f679d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9b52b5eceb85473967d7048b37b8dc",
+    "url": "/currency-converter-react-app/index.html"
+  },
+  {
+    "revision": "60cad2100de9565d801c",
+    "url": "/currency-converter-react-app/static/css/main.1c3cb6b5.chunk.css"
+  },
+  {
+    "revision": "67840e1bfca0372ffc02",
+    "url": "/currency-converter-react-app/static/js/2.dc4f2694.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/currency-converter-react-app/static/js/2.dc4f2694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cad2100de9565d801c",
+    "url": "/currency-converter-react-app/static/js/main.e0c4db99.chunk.js"
+  },
+  {
+    "revision": "890d4b53ce37b153fc34",
+    "url": "/currency-converter-react-app/static/js/runtime-main.d5581ad5.js"
+  }
+]);
